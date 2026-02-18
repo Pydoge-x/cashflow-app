855f7217648a2059f3256521a6174cce8ed86891
Pydoge <18769180912@163.com>
2026-02-19 03:25:48 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is ahead of 'origin/master' by 1 commit.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	modified:   .gitignore
    	modified:   fronted/index.html
    	modified:   fronted/src/router/index.js
    	modified:   fronted/vite.config.js

--- fronted/vite.config.js
+++ fronted/vite.config.js
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  base: './',
+  plugins: [vue(), legacy({
+    targets: ['defaults', 'Android >= 4.4'],
+    additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+  })],
   server: {
     host: '0.0.0.0',
     port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:1800',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   }
 })
