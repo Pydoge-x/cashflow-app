94fda6c86fba90ddb827958613b63511710ceead
Pydoge <18769180912@163.com>
2026-02-21 04:25:22 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	deleted:    fronted/docker-compose.yml
    	modified:   fronted/vite.config.js

--- fronted/vite.config.js
+++ fronted/vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import legacy from '@vitejs/plugin-legacy'
+import { loadEnv } from 'vite'
+
+const env = loadEnv('development', process.cwd())
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,6 +13,7 @@ export default defineConfig({
     additionalLegacyPolyfills: ['regenerator-runtime/runtime']
   })],
   server: {
+    allowedHosts: env.VITE_SERVER_ALLOWED_HOSTS?.split(',') || [],
     host: '0.0.0.0',
     port: 5173,
     proxy: {
